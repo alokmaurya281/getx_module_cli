9194206fc5a84cbcd49caf52e9b08b8b66acb20f
w3villa <w3villa@w3villas-MacBook-Air.local>
2025-04-30 15:49:34 +0530

    intial

---
+++ README.md
@@ -0,0 +1,8 @@
+# GetX Module CLI
+
+A Dart CLI tool to quickly scaffold GetX modules (bindings, views, controllers, providers) for Flutter apps.
+
+## Install
+
+```bash
+dart pub global activate getx_module_cli
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  if (args.isEmpty) {
+    print('❌ Please provide a module name.');
+    print('Usage: getx_module_cli <module_name>');
+    exit(1);
+  }
+
+  final moduleName = args[0].toLowerCase();
+  final capModuleName = moduleName[0].toUpperCase() + moduleName.substring(1);
+
+  final basePath = 'lib/modules/$moduleName';
+  final paths = [
+    '$basePath/controllers',
+    '$basePath/views',
+    '$basePath/bindings',
+    '$basePath/providers',
+  ];
+
+  for (var path in paths) {
+    Directory(path).createSync(recursive: true);
+  }
+
+  File('$basePath/bindings/${moduleName}_binding.dart').writeAsStringSync('''
+import 'package:get/get.dart';
+import '../controllers/${moduleName}_controller.dart';
+
+class ${capModuleName}Binding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<${capModuleName}Controller>(() => ${capModuleName}Controller());
+  }
+}
+''');
+
+  File('$basePath/controllers/${moduleName}_controller.dart').writeAsStringSync('''
+import 'package:get/get.dart';
+
+class ${capModuleName}Controller extends GetxController {
+  // TODO: Add controller logic
+}
+''');
+
+  File('$basePath/views/${moduleName}_view.dart').writeAsStringSync('''
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/${moduleName}_controller.dart';
+
+class ${capModuleName}View extends GetView<${capModuleName}Controller> {
+  const ${capModuleName}View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('${capModuleName} View')),
+      body: Center(child: Text('Welcome to ${capModuleName} Module')),
+    );
+  }
+}
+''');
+
+  File('$basePath/providers/${moduleName}_provider.dart').writeAsStringSync('''
+// This is your API service layer for $moduleName module
+class ${capModuleName}Provider {
+  // TODO: Implement API methods
+}
+''');
+
+  print('✅ Module "$moduleName" created at lib/modules/$moduleName');
+}
+// This script creates a basic structure for a GetX module in Flutter.
